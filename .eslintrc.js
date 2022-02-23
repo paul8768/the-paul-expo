5dfee5597866fab4efeb98accf0349ee3e56c3ac
paul8768 <tripleji8768@gmail.com>
2022-02-23 14:02:24 +0900

    add. lint & prettier 세팅중

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,21 +8,22 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   // ECMScript 규격의 JSX 사용 여부
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018,
     sourceType: 'module',
     project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   // 서드파티 플러그인
-  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'unused-imports'],
   // 추가한 플러그인에서 사용할 규칙
   extends: [
     'airbnb',
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   // 규칙
@@ -33,5 +34,15 @@ module.exports = {
     'react/prop-types': 0,
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
+    'react/jsx-filename-extension': [warn, { extensions: ['.js', '.jsx', 'tsx', 'ts'] }],
+    'prettier/prettier': 'error', // Error when breaking Prettier rules
+    'import/no-duplicates': 'error', // no-duplicate-imports autofix
+    'react-native/no-inline-styles': 0,
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
   },
 };
