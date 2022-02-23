96cd8a7a27f6d4684ef25e34b31a9658571241e9
paul8768 <tripleji8768@gmail.com>
2022-02-23 14:47:50 +0900

    add. 이미지 추가

---
+++ src/resources/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  whiteMode: {
+    basicBackground: '#ffffff',
+  },
+  darkMode: {
+    basicBackground: '#000000',
+    orangeColor: '#fec763',
+    pinkColor: '#ea55b1',
+    mintColor: '#79fffe',
+    purpleColor: '#a992fa',
+    whiteColor: '#ffffff',
+  },
+};
---
+++ src/resources/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable global-require */
+const images = {
+  circle: require('./images/circle.png'),
+  triangle: require('./images/triangle.png'),
+  square: require('./images/square.png'),
+};
+
+export default { images };
